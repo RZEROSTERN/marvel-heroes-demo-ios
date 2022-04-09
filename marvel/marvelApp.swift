a6345440fd860a7017d3ad3fef65939aeb9d8bb0
Marco Ramírez <marco.rs@wizeline.com>
2022-04-08 23:28:56 -0500

    Initial Commit

---
+++ marvel/marvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvelApp.swift
+//  marvel
+//
+//  Created by Marco Ramírez Solís on 08/04/22.
+//
+
+import SwiftUI
+
+@main
+struct marvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
